5f6807ee4fefec98cbd87554667c94e87e61cf14
Nikita <nekit5553535@gmail.com>
2026-03-30 20:05:01 +0300

    Initial Commit

---
+++ Cleaner/CleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanerApp.swift
+//  Cleaner
+//
+//  Created by Nykyta Kasianenko on 30.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
